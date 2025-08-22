a5bba63b59048a89783392868e0226444ca4cff0
abubakar0320 <abubakr.bgnu@gmail.com>
2025-08-22 12:39:36 +0500

    chore: migrate to ESLint flat config, ignore vendor, fix lint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,37 @@
-import js from '@eslint/js';
-import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: [
+      'vendor/**',
+      'node_modules/**',
+      'dist/**',
+    ],
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      env: {
+        browser: true,
+        es2021: true,
+      },
     },
     plugins: {
+      '@typescript-eslint': {},
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended'
+    ],
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
@@ -24,5 +39,19 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  }
-);
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      env: {
+        browser: true,
+        es2021: true,
+      },
+    },
+    extends: ['eslint:recommended'],
+  },
+];
